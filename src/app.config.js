ea4e651d1e318b02e0c5da2903268511dab140f7
BsKPLu <bianshuiguangzhong@gmail.com>
2024-12-06 23:42:49 +0800

    2024-12-06

--- src/app.config.js
+++ src/app.config.js
@@ -7,5 +7,11 @@ export default defineAppConfig({
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  permission: {
+    'scope.userLocation': {
+      desc: '您的位置信息将用于小程序位置接口的效果展示',
+    },
+  },
+  requiredPrivateInfos: ['getLocation'],
 })
